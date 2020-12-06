40dde90d4193003294a5edb6e4df0fe1b74c7f68
Devon Wolfkiel <devonwolf@tutanota.com>
2020-12-06 10:53:12 -0800

    Clean up unused code

--- scrap.js
+++ scrap.js
@@ -1,37 +1,27 @@
 // Code Scrapyard
-/*
-function planDecrease(startCount, decCount, divisions) {
-	let round = 0;
-	let sections = [];
-	if (startCount % divisions === 0) {
-		for (let i = 0; i < divisions - 1; i++) {
-			sections.push(startCount / divisions);
-		};
+// CODE BELOW IS STILL SCRAP
+	/* else if (!desiredSections % 2 === 0) {
+		console.log('This is an odd number of desiredSections, please start over with an even number.');
 	}
-	else if (!divisions % 2 === 0) {
-		console.log('This is an odd number of divisions, please start over with an even number.');
-	}
-	else if (startCount % 2 === 0) {
+	else if (stitchCount % 2 === 0) {
 		// divide up sections reasonably and distribute stray stitches
-		let baseSection = startCount - (startCount % divisions);
-		for (let i = 0; i < divisions - 2; i++) {
-			sections.push(baseSection / divisions);
+		let baseSection = stitchCount - (stitchCount % desiredSections);
+		for (let i = 0; i < desiredSections - 2; i++) {
+			sections.push(baseSection / desiredSections);
 		};
-		sections.push(startCount % divisions);
-		// example: [6, 6, 6, 6, 6, 4] (startCount 34, divisions 6)
+		sections.push(stitchCount % desiredSections);
+		// example: [6, 6, 6, 6, 6, 4] (stitchCount 34, desiredSections 6)
 
 		// [6, 5, 6, 6, 5, 6] < that might be an ideal balance
 		// [6, 6, 6, 6, 6, 4] < plus two to final index, minus one from second and next to last
 
-		/*for (let factor = divisions - 2; factor > 2; factor -= 2) {
-			if (startCount % factor === 0) {
-				let baseSectionCount = startCount / factor;
+		for (let factor = desiredSections - 2; factor > 2; factor -= 2) {
+			if (stitchCount % factor === 0) {
+				let baseSectionCount = stitchCount / factor;
 
 			}
 		}
-	}
+	} 
 	else {
 		// distribute stray stitch and then go back up to the else if probably
-	}
-}
-*/
+	}*/
--- utils.js
+++ utils.js
@@ -19,31 +19,5 @@ export function planSections(stitchCount, desiredSections) {
 		};
 		return sections;
 		// test: 100 stitches, 4 desired sections should return this array: [25, 25, 25, 25] - PASSED
-	}
-	// CODE BELOW IS STILL SCRAP
-	else if (!desiredSections % 2 === 0) {
-		console.log('This is an odd number of desiredSections, please start over with an even number.');
-	}
-	else if (stitchCount % 2 === 0) {
-		// divide up sections reasonably and distribute stray stitches
-		let baseSection = stitchCount - (stitchCount % desiredSections);
-		for (let i = 0; i < desiredSections - 2; i++) {
-			sections.push(baseSection / desiredSections);
-		};
-		sections.push(stitchCount % desiredSections);
-		// example: [6, 6, 6, 6, 6, 4] (stitchCount 34, desiredSections 6)
-
-		// [6, 5, 6, 6, 5, 6] < that might be an ideal balance
-		// [6, 6, 6, 6, 6, 4] < plus two to final index, minus one from second and next to last
-
-		/*for (let factor = desiredSections - 2; factor > 2; factor -= 2) {
-			if (stitchCount % factor === 0) {
-				let baseSectionCount = stitchCount / factor;
-
-			}
-		}*/
-	}
-	/*else {
-		// distribute stray stitch and then go back up to the else if probably
-	}*/
+	};
 };	
